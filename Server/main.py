eabc3dd80c77553ef80cba7e32b5ce123eb9c649
Pk_Patel <parthpatel9414@gmail.com>
2022-12-04 18:55:14 -0500

    changed the file

--- Server/main.py
+++ Server/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+import json
+from operations import getAllimmdieateChildren
 
+# initiating the app
 app = FastAPI()
+
+# loading the directory from Json File
+crumbsData = json.load(open('../src/directory.json'))
 
 @app.get("/")
 def root():
-    return {"Hello" : "There!!"}
+    children = getAllimmdieateChildren(crumbsData)
+    return children
