01f37c4ce3750d46f9dff677b9642ea1fa8b76cc
Pk Patel <parthpatel9414@gmail.com>
2022-12-04 22:42:43 +0000

    Loaded the file in main.py

--- Server/src/main.py
+++ Server/src/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+import json
 
+# initiating the app
 app = FastAPI()
+
+# loading the directory from Json File
+crumbsData = json.load(open('../src/directory.json'))
 
 @app.get("/")
 def root():
-    return {"Hello" : "There!!"}
+    return crumbsData
