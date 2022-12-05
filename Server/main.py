0d029d0e7de981f54b42b01512961767345d38c1
Pk Patel <parthpatel9414@gmail.com>
2022-12-05 23:50:24 +0000

    Fixed the api which can fetch dynamic routes and gives results

--- Server/main.py
+++ Server/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, APIRouter, Request
 import re
 import json
-from operations import getAllimmdieateChildren
+from operations import getfirstchild, findkeys
 
 # initiating the app
 app = FastAPI()
@@ -12,15 +12,30 @@ my_router = APIRouter(prefix="/path")
 # loading the directory from Json File
 crumbsData = json.load(open('./directory.json'))
 
-
-
 @app.api_route("/{path_name:path}", methods=["GET"])
 async def catch_all(path_name: str):
+    result = {}
+    if not path_name:
+        return {"details" :  "Not Found"}
+    
+    if "path" == path_name:
+        return getfirstchild(data=crumbsData)
+    
+    path_values = list()
+    
+
     if path_name and '/' in path_name:
         path_values = path_name.split("/")[1:]
-    return getAllimmdieateChildren(crumbsData)
+    else: 
+        path_values = ["home"]
+    
+    immediate_child = []    
+    last_node = path_values[-1]
 
-# @app.get("/path/{route_path}")
-# async def get_path(route_path :str):
-     
-#     return getAllimmdieateChildren(crumbsData, pathString = route_path)
+    for value in findkeys(crumbsData,last_node):
+        result["type"] = value["type"]
+        for child_key, child_value in value.items():
+            if child_key == "children":
+                immediate_child.extend(child_value.keys())
+    result["children"] = immediate_child
+    return result
--- Server/operations.py
+++ Server/operations.py
@@ -1,30 +1,23 @@
-
-# def getAllimmdieateChildren(data, path_values : list()):
-#     children = list()
-
-#     for key in data.keys(): 
-#         if key == "children": 
-#             children.append(*data[key].keys())
-
-# # check if children already exist in path
-#             path_len = len(path_values)
-#             for i in path_values: 
-#                 child_data = data[key].values()
-#                 print(getAllimmdieateChildren(data[key][i], path_values = [i]))
-#                 # children.append(*getAllimmdieateChildren(data[key][i],i))
-#     return children
-
-def getAllimmdieateChildren(data):
-    children = list()
-
+def getfirstchild(data):
+    result = {}
     for key, value in data.items(): 
         if key == "type" and value == "dir":
-            child = [*data["children"].keys()]
-            print(child)
-            # children.extend(*data["children"].keys())
-        if isinstance(value, dict):
-            getAllimmdieateChildren(value)
-            # print(key, ": ", value)
-            
-    return children
+            result[key] = value
+            result["children"] = [*data["children"].keys()]
+            return result
+
+def findkeys(node, kv):
+    if isinstance(node, list):
+        for i in node:
+            for x in findkeys(i, kv):
+               yield x
+    elif isinstance(node, dict):
+        if kv in node:
+            yield node[kv]
+        for j in node.values():
+            for x in findkeys(j, kv):
+                yield x
+
+
+
 
