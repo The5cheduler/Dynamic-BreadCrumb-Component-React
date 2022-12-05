a351e3f3bb407d6efc049a4209fd7c52719480d5
Pk Patel <parthpatel9414@gmail.com>
2022-12-05 18:29:15 +0000

    developed traversing algo for directory

--- Server/main.py
+++ Server/main.py
@@ -12,9 +12,13 @@ my_router = APIRouter(prefix="/path")
 # loading the directory from Json File
 crumbsData = json.load(open('./directory.json'))
 
+
+
 @app.api_route("/{path_name:path}", methods=["GET"])
 async def catch_all(path_name: str):
-    return getAllimmdieateChildren(crumbsData, path_name)
+    if path_name and '/' in path_name:
+        path_values = path_name.split("/")[1:]
+    return getAllimmdieateChildren(crumbsData)
 
 # @app.get("/path/{route_path}")
 # async def get_path(route_path :str):
--- Server/operations.py
+++ Server/operations.py
@@ -1,14 +1,30 @@
 
-def getAllimmdieateChildren(data, pathString : str = None):
+# def getAllimmdieateChildren(data, path_values : list()):
+#     children = list()
+
+#     for key in data.keys(): 
+#         if key == "children": 
+#             children.append(*data[key].keys())
+
+# # check if children already exist in path
+#             path_len = len(path_values)
+#             for i in path_values: 
+#                 child_data = data[key].values()
+#                 print(getAllimmdieateChildren(data[key][i], path_values = [i]))
+#                 # children.append(*getAllimmdieateChildren(data[key][i],i))
+#     return children
+
+def getAllimmdieateChildren(data):
     children = list()
-    path_values = pathString.split("/")  
-    for key in data.keys(): 
-        if key == "children": 
-            children.append(*data[key].keys())
-# check if children already exist in path
-        if not pathString: 
-            print(*getAllimmdieateChildren(data[key][pathString]))
-            children.append(*getAllimmdieateChildren(data[key][pathString])) 
-                  
+
+    for key, value in data.items(): 
+        if key == "type" and value == "dir":
+            child = [*data["children"].keys()]
+            print(child)
+            # children.extend(*data["children"].keys())
+        if isinstance(value, dict):
+            getAllimmdieateChildren(value)
+            # print(key, ": ", value)
+            
     return children
 
