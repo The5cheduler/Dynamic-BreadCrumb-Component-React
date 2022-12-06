1bd8547ac91a13921df44c55d31463374e910ebf
Pk Patel <parthpatel9414@gmail.com>
2022-12-06 03:08:35 +0000

    h

--- Client/dynamic-breadcrumbs/src/App.js
+++ Client/dynamic-breadcrumbs/src/App.js
@@ -7,12 +7,18 @@ import Breadcrumb from './components/Breadcrumb';
 
 function App() {
 
-  const fetchcrumbs = async() => {
-    const response = await fetch("http://localhost:8000/todo")
+   // assign value of crumbs here
+   const [data, setdata] = useState([]); 
+   const [crumbs, setcrumbs] = useState([]); 
+
+  const fetchdata = async() => {
+    const response = await fetch("http://localhost:8000/path")
+    const data = await response.json()
+    setdata(data.data);
+    
   }
 
-  // assign value of crumbs here
-  const [crumbs, setcrumbs] = useState(["home", "myname"]); 
+ 
 
   const selected = crumb => {
     console.log(crumb)
